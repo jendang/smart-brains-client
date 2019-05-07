bb0424a1e09f1b5597daa9330c093a6272f160cd
jennydang <jenny.dang117@gmail.com>
2019-05-07 19:43:29 +0200

    one face detection

--- src/App.js
+++ src/App.js
@@ -1,20 +1,96 @@
 import React from 'react';
+import Particles from 'react-particles-js'; //animate bg
+import Clarifai from 'clarifai' //for face-dection API
+
+
 import Navigation from './components/Navigation/Navigation'
 import Logo from './components/Logo/Logo'
+import Rank from './components/Rank/Rank'
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
 import FaceRecognition from './components/FaceRecognition/FaceRecognition'
 import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <Navigation />
-      <Logo />
-      <ImageLinkForm />
-      <FaceRecognition />
-      
-    </div>
-  );
+const particlesOption = {
+  particles: {
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    },
+  }
+}
+
+const app = new Clarifai.App({
+  apiKey: 'b9a8ed341183472b90520b9da5aa9cbb'
+});
+
+
+class App extends React.Component {
+  state = {
+    input: '',
+    imageUrl: '',
+    box: {}
+  }
+
+  calculateFaceLocation = (data) => {
+    const faceDetection = data.outputs[0].data.regions[0].region_info.bounding_box
+    const image = document.getElementById('inputimage')
+    const width = Number(image.width)
+    const height = Number(image.height)
+    //console.log(width, height)
+    //console.log(faceDetection)
+    return {
+      leftCol: faceDetection.left_col * width,
+      topRow: faceDetection.top_row * height,
+      rightCol: width - (faceDetection.right_col * width),
+      bottomRow: height - (faceDetection.bottom_row * height)
+    }
+  }
+
+  displayFaceBox = (box) => {
+    //console.log(box)
+    this.setState({ box: box })
+  }
+
+  onInputChange = (event) => {
+    //console.log(event.target.value)
+    this.setState({ input: event.target.value })
+  }
+
+  onSubmit = () => {
+    this.setState({ imageUrl: this.state.input })
+    app.models
+      .predict(
+        Clarifai.FACE_DETECT_MODEL,
+        this.state.input
+      )
+      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+      .catch(err => console.log(err))
+     
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <Particles params={particlesOption} className="particles" />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm 
+          onInputChange={this.onInputChange} 
+          onButtonSubmit={this.onSubmit}
+        />
+        <FaceRecognition 
+          imageUrl={ this.state.imageUrl }
+          box={this.state.box}
+        />
+        
+      </div>
+    );
+
+  }
 }
 
 export default App;
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,14 +1,26 @@
 import React from 'react'
+import './ImageLinkForm.css'
 
-const ImageLinkForm = () => {
+const ImageLinkForm = (props) => {
     return(
         <div>
             <p className="f3">
                 This Magic Brain will detect faces in your pictures, give it a try! 
             </p>
             <div className="center">
-                <input className="f4 pa2 w-60 center" type="text" />
-                <button className="w-20 grow f4 link ph3 pv2 dib white bg-light-purple">Detect</button>
+                <div className="form center pa4 br3 shadow-5">
+                    <input 
+                        onChange={props.onInputChange}
+                        className="f4 pa2 w-70 center" 
+                        type="text" 
+                    />
+                    <button 
+                        className="w-20 grow f4 link ph3 pv2 dib white bg-light-purple"
+                        onClick={props.onButtonSubmit}
+                    >
+                        Detect
+                    </button>
+                </div>
             </div>
         </div>
     )
